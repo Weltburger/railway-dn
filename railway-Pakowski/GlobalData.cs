7039236789ddfe109c0b9aecc10a4948f81581a8
Weltburger <pakowski398@yandex.ru>
2020-06-12 15:32:35 +0300

    Pakowski's tables done

---
+++ railway-Pakowski/GlobalData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace test_railway
+{
+    static class GlobalData
+    {
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        public static string sql;
+        public static Excel.Application excelApp;
+        public static Excel.Workbook workBook;
+    }
+}
